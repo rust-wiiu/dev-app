742b7895bc1672af9f461377e7a9a9d0a05ade23
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-04-20 23:32:21 +0200

    dev

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use wut::gx2::shader::{self, attribute::Float4};
 
 #[derive(ShaderAttributes)]
 struct MyShader {
-    #[name = "aPosition"]
+    #[name = "in_position"]
     // #[index = 0]
     // #[offset = 3]
     a_position: shader::Attribute<Float4>,
-    #[name = "aColour"]
+    #[name = "in_color"]
     // #[index = 1]
     // #[offset = 0]
     a_color: shader::Attribute<Float4>,
 }
 
-static PROGRAM: shader::Program = shader::Program::from(include_bytes!("shader.gsh"));
+static PROGRAM: shader::Program = shader::Program::from(include_bytes!("out.gsh"));
 
 /*
 &[
@@ -118,10 +118,10 @@ static PROGRAM: shader::Program = shader::Program::from(include_bytes!("shader.g
 ]
 */
 
-#[wut_main(Cafe)]
-fn main() {
-    use wut::bindings::*;
+extern crate cafeglsl;
 
+#[main(Cafe, Console)]
+fn main() {
     let mut shader: shader::Shader<MyShader> = shader::Shader::new(0, &PROGRAM).unwrap();
 
     let mut position_buffer = shader::Buffer::from(
@@ -188,7 +188,7 @@ fn main() {
             .set_buffer(&mut position_buffer);
         shader.attributes.a_color.set_buffer(&mut color_buffer);
 
-        shader.render();
+        shader.render(&context);
 
         let context = context.drc();
 
@@ -196,188 +196,4 @@ fn main() {
 
         context.finish();
     }
-
-    // unsafe {
-    //     /*
-    //     let mut group = WHBGfxShaderGroup::default();
-
-    //     if WHBGfxLoadGFDShaderGroup(&mut group, 0, SHADER.as_ptr() as *const _) == 0 {
-    //         return;
-    //     }
-    //     */
-
-    //     // let group = ShaderGroup::new(0, &SHADER).unwrap();
-
-    //     // WHBGfxInitShaderAttribute(
-    //     //     &mut group,
-    //     //     c"aPosition".as_ptr(),
-    //     //     0,
-    //     //     0,
-    //     //     GX2AttribFormat::GX2_ATTRIB_FORMAT_FLOAT_32_32_32_32,
-    //     // );
-    //     // WHBGfxInitShaderAttribute(
-    //     //     &mut group,
-    //     //     c"aColour".as_ptr(),
-    //     //     1,
-    //     //     0,
-    //     //     GX2AttribFormat::GX2_ATTRIB_FORMAT_FLOAT_32_32_32_32,
-    //     // );
-    //     // WHBGfxInitFetchShader(&mut group);
-
-    //     /*
-    //     {
-    //         use GX2RResourceFlags as F;
-
-    //         position_buffer.flags = F::GX2R_RESOURCE_BIND_VERTEX_BUFFER
-    //             | F::GX2R_RESOURCE_USAGE_CPU_READ
-    //             | F::GX2R_RESOURCE_USAGE_CPU_WRITE
-    //             | F::GX2R_RESOURCE_USAGE_GPU_READ;
-    //         position_buffer.elemSize = 4 * 4;
-    //         position_buffer.elemCount = 3;
-    //         GX2RCreateBuffer(&mut position_buffer);
-    //         let buffer = GX2RLockBufferEx(&mut position_buffer, 0) as *mut f32;
-    //         buffer.copy_from(
-    //             [
-    //                 1.0, -1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, -1.0, -1.0, 1.0, 1.0,
-    //             ]
-    //             .as_ptr(),
-    //             4 * 3,
-    //         );
-    //         GX2RUnlockBufferEx(&mut position_buffer, 0);
-
-    //         color_buffer.flags = F::GX2R_RESOURCE_BIND_VERTEX_BUFFER
-    //             | F::GX2R_RESOURCE_USAGE_CPU_READ
-    //             | F::GX2R_RESOURCE_USAGE_CPU_WRITE
-    //             | F::GX2R_RESOURCE_USAGE_GPU_READ;
-    //         color_buffer.elemSize = 4 * 4;
-    //         color_buffer.elemCount = 3;
-    //         GX2RCreateBuffer(&mut color_buffer);
-    //         let buffer = GX2RLockBufferEx(&mut color_buffer, 0) as *mut f32;
-    //         buffer.copy_from(
-    //             [1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0].as_ptr(),
-    //             4 * 3,
-    //         );
-    //         GX2RUnlockBufferEx(&mut color_buffer, 0);
-    //     }
-    //     */
-    //     while process::running() {
-    //         /*
-    //         {
-    //             let colors = core::slice::from_raw_parts_mut(
-    //                 GX2RLockBufferEx(&mut color_buffer, 0) as *mut f32,
-    //                 12,
-    //             );
-    //             colors[0] = 1.0;
-    //             colors[1] = if colors[1] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[1] + 0.01
-    //             };
-    //             colors[2] = if colors[2] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[2] + 0.01
-    //             };
-    //             colors[3] = 1.0;
-    //             // ------------------------------
-    //             colors[4] = if colors[4] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[4] + 0.01
-    //             };
-    //             colors[5] = 1.0;
-    //             colors[6] = if colors[6] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[6] + 0.01
-    //             };
-    //             colors[7] = 1.0;
-    //             // ------------------------------
-    //             colors[8] = if colors[8] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[8] + 0.01
-    //             };
-    //             colors[9] = if colors[9] >= 1.0 {
-    //                 0.0
-    //             } else {
-    //                 colors[9] + 0.01
-    //             };
-    //             colors[10] = 1.0;
-    //             colors[11] = 1.0;
-
-    //             GX2RUnlockBufferEx(&mut color_buffer, 0);
-    //         }
-    //          */
-
-    //         /*
-    //         {
-    //             let mut b = color_buffer.lock().unwrap();
-    //             let i = 0;
-    //             b[i].x = 1.0;
-    //             b[i].y = if b[i].y >= 1.0 { 0.0 } else { b[i].y + 0.01 };
-    //             b[i].z = if b[i].z >= 1.0 { 0.0 } else { b[i].z + 0.01 };
-    //             b[i].w = 1.0;
-
-    //             let i = 1;
-    //             b[i].x = if b[i].x >= 1.0 { 0.0 } else { b[i].x + 0.01 };
-    //             b[i].y = 1.0;
-    //             b[i].z = if b[i].z >= 1.0 { 0.0 } else { b[i].z + 0.01 };
-    //             b[i].w = 1.0;
-
-    //             let i = 2;
-    //             b[i].x = if b[i].x >= 1.0 { 0.0 } else { b[i].x + 0.01 };
-    //             b[i].y = if b[i].y >= 1.0 { 0.0 } else { b[i].y + 0.01 };
-    //             b[i].z = 1.0;
-    //             b[i].w = 1.0;
-    //         }
-    //         */
-
-    //         WHBGfxBeginRender();
-
-    //         // println!("begin render");
-
-    //         WHBGfxBeginRenderTV();
-    //         WHBGfxClearColor(0.0, 0.0, 1.0, 1.0);
-    //         // GX2RSetAttributeBuffer(
-    //         //     position_buffer.as_raw_mut(),
-    //         //     0,
-    //         //     core::mem::size_of::<Float4>() as u32,
-    //         //     0,
-    //         // );
-    //         // GX2RSetAttributeBuffer(
-    //         //     color_buffer.as_raw_mut(),
-    //         //     1,
-    //         //     core::mem::size_of::<Float4>() as u32,
-    //         //     0,
-    //         // );
-    //         // GX2SetFetchShader(&group.fetchShader);
-    //         // GX2SetVertexShader(group.vertexShader);
-    //         // GX2SetPixelShader(group.pixelShader);
-    //         // GX2DrawEx(GX2PrimitiveMode::GX2_PRIMITIVE_MODE_TRIANGLES, 3, 0, 1);
-    //         WHBGfxFinishRenderTV();
-
-    //         WHBGfxBeginRenderDRC();
-    //         WHBGfxClearColor(1.0, 0.0, 1.0, 1.0);
-    //         // GX2RSetAttributeBuffer(
-    //         //     position_buffer.as_raw_mut(),
-    //         //     0,
-    //         //     core::mem::size_of::<Float4>() as u32,
-    //         //     0,
-    //         // );
-    //         // GX2RSetAttributeBuffer(
-    //         //     color_buffer.as_raw_mut(),
-    //         //     1,
-    //         //     core::mem::size_of::<Float4>() as u32,
-    //         //     0,
-    //         // );
-    //         // GX2SetFetchShader(&group.fetchShader);
-    //         // GX2SetVertexShader(group.vertexShader);
-    //         // GX2SetPixelShader(group.pixelShader);
-    //         // GX2DrawEx(GX2PrimitiveMode::GX2_PRIMITIVE_MODE_TRIANGLES, 3, 0, 1);
-    //         WHBGfxFinishRenderDRC();
-
-    //         WHBGfxFinishRender();
-    //     }
-    // }
 }
